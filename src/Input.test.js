892dc30e3126d408fccf29f3d12a309a26ec42e7
adamtowers <hello@adamtowers.tech>
2020-01-22 12:22:40 -0500

    wrote tests for Input success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -38,9 +38,23 @@ describe("render", () => {
     });
   });
   describe("word has been guessed", () => {
-    test("renders the component without error", () => {});
-    test("does not render input box", () => {});
-    test("does not render submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test("renders the component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("does not render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+    test("does not render submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
