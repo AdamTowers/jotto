65e26b960f6f66c9a010983873aa3de1fa6d3352
adamtowers <hello@adamtowers.tech>
2020-01-22 12:09:56 -0500

    completed Input component render tests for success=false

--- src/Input.js
+++ src/Input.js
@@ -3,12 +3,29 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <div></div>;
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="mb-2 mx-sm-3"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{contents}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
--- src/Input.test.js
+++ src/Input.test.js
@@ -19,9 +19,23 @@ const setup = (initialState = {}) => {
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders the component without error", () => {});
-    test("renders input box", () => {});
-    test("renders submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders the component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
   });
   describe("word has been guessed", () => {
     test("renders the component without error", () => {});
