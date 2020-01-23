822427bce0ab10d654fb48c2eae284093c351dcb
adamtowers <hello@adamtowers.tech>
2020-01-23 17:18:42 -0500

    set up guessedWords tests

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function guessedWordsReducer
+ * @param {array} state – Array of guessed words.
+ * @param {object} action – Action to be reduced.
+ * @returns {array} – New guessedWords state.
+ */
+export default (state, action) => {
+  return null;
+};
