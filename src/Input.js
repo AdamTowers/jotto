958364281b7fb3d3b195733458b073eb82f4c87d
adamtowers <hello@adamtowers.tech>
2020-01-31 16:43:45 -0500

    added test for clearing the input box on guessWord

--- src/Input.js
+++ src/Input.js
@@ -27,6 +27,8 @@ export class UnconnectedInput extends Component {
     if (guessedWord && guessedWord.length > 0) {
       this.props.guessWord(guessedWord);
     }
+
+    this.setState({ currentGuess: "" });
   }
 
   render() {
