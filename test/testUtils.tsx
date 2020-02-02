0979555b32c4a94c6612f0a2b408ee2ee01e090a
adamtowers <hello@adamtowers.tech>
2020-02-02 16:07:58 -0500

    added typescript to project and renamed files

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -26,14 +26,3 @@ export const storeFactory = initialState => {
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
-
-// Todo: Delete this test once the project is migrated to TypeScript.
-export const checkProps = (component, conformingProps) => {
-  const propError = checkPropTypes(
-    component.propTypes,
-    conformingProps,
-    "prop",
-    component.name
-  );
-  expect(propError).toBeUndefined();
-};
